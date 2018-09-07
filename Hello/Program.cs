248124e7f01a756d747dfe14211dcf120cd1274e
vic1vega <janusz.pacholski@outlook.com>
2018-09-07 17:56:50 +0200

    Zadanie #6 - funkcje konsolowe i pobieranie danych od użytkownika

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -6,15 +6,70 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            string name = args[0];
+            valueTypes();
 
-            Console.WriteLine(name);
+            for (; ; )
+            {
+                PodajImie();
+                PodajWiek();
+                Ustawienia();
+            }
+        }
+
+        private static void valueTypes()
+        {
+            int maxInt = int.MaxValue;
+            int minInt = int.MinValue;
+            long maxLong = long.MaxValue;
+            long minLong = long.MinValue;
+
+            Console.WriteLine("maxInt= " + maxInt);
+            Console.WriteLine("minInt= " + minInt);
+            Console.WriteLine("maxLong= " + maxLong);
+            Console.WriteLine("minLong= " + minLong);
+        }
+
+        /// <summary>
+        /// Czyści konsolę do stanu 0
+        /// </summary>
+        private static void Ustawienia()
+        {
             Console.ReadKey();
-            //foreach (var item in args)
-            //{
-            //    Console.WriteLine(item);
-            //}
+            Console.Clear();
+            Console.ResetColor();
+        }
+        
+        /// <summary>
+        /// Wypisujemy komunikat zależny od podanego wieku użytkownika
+        /// </summary>
+        private static void PodajWiek()
+        {
+            Console.Write("Podaj swój wiek: ");
+            bool result = int.TryParse(Console.ReadLine(), out int age);
 
+            if (age > 18)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine("Jesteś pełnoletni. Możesz wypić piwsko.");
+            }
+            else if (result == false)
+            {
+                Console.WriteLine("Wiek jest podany niepoprawnie.");
+            }
+            else
+            {
+                Console.WriteLine("Dla Ciebie Młody kawalerze mamy mleko.");
+            }
+        }
+
+        /// <summary>
+        /// Wypisujemy imię użytkownika
+        /// </summary>
+        private static void PodajImie()
+        {
+            Console.Write("Wpisz swoje Imię: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Witaj " + name);
         }
     }
 }
