74e8d49ed15859ee76daf00d9739da94d03a0f11
vic1vega <janusz.pacholski@outlook.com>
2018-09-08 08:59:01 +0200

    Zadanie #7 - utworzenie pierwszej klasy (Diary) + implementacja pól i metod dla tej klasy

---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        //Stan (zmienne - pola)
+        List<float> ratings;
+
+
+        //Zachowania (metody, którymi będzie dysponować klasa) 
+
+        public void AddRating (float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float CalculateAverage()
+        {
+            float sum = 0, avg = 0;
+
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+
+            avg = sum / ratings.Count;
+            return avg;
+        }
+
+        public float GetMaxRating()
+        {
+            return ratings.Max();
+        }
+
+        public float GetMinRating()
+        {
+            return ratings.Min();
+        }
+    }
+}
---
+++ StudentDiary/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentDiary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
