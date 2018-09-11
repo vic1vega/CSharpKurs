e849a88a8d40b14c7975a9be87574a7dd0cf8ee4
vic1vega <janusz.pacholski@outlook.com>
2018-09-11 12:47:04 +0200

    Zadanie #23 - członkowie(members) metod i klas + słówko params pozwalające korzystać z przekazaywania tablicy parametrów

---
+++ Members/Diary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Members
+{
+    public class Diary
+    {
+        //Stan (zmienne - pola)
+        List<float> ratings = new List<float>();
+        public string Name;
+
+        //Zachowania (metody, którymi będzie dysponować klasa) 
+
+        /// <summary>
+        /// Dodaje ocenę do zbioru ocen w Dzienniczku
+        /// </summary>
+        /// <param name="rating">Nowa ocena</param>
+        public void AddRating (float rating)
+        {
+            if (rating >= 0 && rating <=10)
+            {
+                ratings.Add(rating);
+            }
+           
+        }
+        /// <summary>
+        /// Oblicza statystyki wyseparowane do klasy DiaryStatistics
+        /// </summary>
+        /// <returns></returns>
+        public DiaryStatistics ComputeStatistics()
+        {
+            DiaryStatistics stats = new DiaryStatistics();
+
+            float sum = 0;
+
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+
+            stats.averageGrade = sum / ratings.Count;
+            stats.maxGrade = ratings.Max();
+            stats.minGrade = ratings.Min();
+            
+            return stats;
+        }
+    }
+}
---
+++ Members/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Members
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary diary = new Diary();
+            diary.AddRating(7);
+            diary.AddRating(8);
+            diary.AddRating(4.5f);
+            diary.AddRating(8.9f);
+
+            DiaryStatistics stats = diary.ComputeStatistics();
+
+            WriteResult("średnia",stats.averageGrade);
+            WriteResult("Max", (int)stats.maxGrade,1);
+            WriteResult("Minimum",stats.minGrade);
+
+            Console.Write("Press any key to quit...");
+            Console.ReadKey();
+        }
+
+        //przykłady overloadingu
+        static void WriteResult(string description,params float[] result)
+        {
+            Console.WriteLine(description + ": " + result[0] + " " + result[1]);
+        }
+
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+
+        static void WriteResult(string description, int result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+    }
+}
